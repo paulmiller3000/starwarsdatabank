6c8b9d17b734ed4acbbdec5327d9a93dc86241a6
Paul Miller <paul@battlestardigital.com>
2018-07-07 14:56:20 -0400

    New favicon, footer, styles

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+	return (
+		<div className='footer tc near-white'>
+			<div>
+				<p>React.Js Portfolio Piece of <span className='center pt3'><a className='near-white link underline-hover' href="https://paulmiller3000.com" target="_blank">Paul Miller</a></span></p>
+			</div>
+		</div>
+	);
+}
+
+export default Footer
