00fb1785fae663453872c35ae5efebe1d8f92d2f
Paul Miller <paul@battlestardigital.com>
2018-07-09 14:25:02 -0400

    Progress towards detail card

--- src/components/StarshipCard.js
+++ src/components/StarshipCard.js
@@ -10,22 +10,49 @@ class StarshipCard extends React.Component {
 	handleChange(e) {
 		e.preventDefault();
 	}
+
+	closeCard(id, event) {	
+		console.log('id');
+		const ele = document.getElementById(id);		
+		const x = document.getElementById(id).children[0];	
+		const detailBtn = ele.querySelector('.card-detail-btn');	
+		const darkDiv = document.createElement('div');
+
+		// Return card to normal state
+		ele.classList.remove('card-enlarged');
+		x.classList.add('hide');
+		detailBtn.classList.remove('hide');
+
+		// Lighten the background
+		document.getElementById('root').removeChild(darkDiv);		
+	}
+
   
 	detailCard(id, event) {			
-		const ele = document.getElementById(id);
-		//const divs = document.querySelectorAll('div:not(id)')		
-		const x = document.getElementById(id).firstChild;		
+		const ele = document.getElementById(id);		
+		const x = document.getElementById(id).children[0];	
+		const detailBtn = ele.querySelector('.card-detail-btn');	
+		const darkDiv = document.createElement('div');
 
-		//divs.classList.add('dark')
-		ele.classList.add('card-enlarged')		
-		x.classList.remove('hide')
+		// Darken the background
+		darkDiv.className = 'dark';
+		document.getElementById('root').appendChild(darkDiv);
+
+		// Enlarge the card, show close button, remove detail link
+		ele.classList.add('card-enlarged');
+		x.classList.remove('hide');
+		detailBtn.classList.add('hide');
 	}
 
 	render() {
 		return (
-				<div id={'card-starship-' + this.props.id} onClick={(e) => this.detailCard('card-starship-' + this.props.id)} className='card tc dib ba br3 pa2 grow bw2 shadow-5 ttc glow'>				
-					<div className='close-btn fr hide'>
-						x
+				<div 
+					id={'card-starship-' + this.props.id} 
+					onClick={(e) => this.detailCard('card-starship-' + this.props.id)} 
+					className='card tc dib ba br3 pa2 grow bw2 shadow-5 ttc glow'
+				>				
+					<div className='close-btn hide'>
+						<p className='fr' onClick={(e) => this.closeCard()}>x</p>
 					</div>
 					<div>
 						<h2>{this.props.name}</h2>
@@ -33,7 +60,7 @@ class StarshipCard extends React.Component {
 						<p className='i'>Manufacturer: {this.props.manufacturer}</p>
 						<p className='card-detail-btn'>[ Detail ]</p>	
 					</div>					
-			        <div class='card-detail hide'>
+			        <div className='card-detail hide'>
 			        	<p>Detail goes here</p>	        	
 			      	</div>						
 				</div>
